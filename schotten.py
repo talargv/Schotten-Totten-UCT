bd31e6b896f1d6fa7ef0163a630589ebefc1a683
Tal <argovtal@gmail.com>
2023-06-02 15:58:56 +0300

    62202316

--- schotten.py
+++ schotten.py
@@ -1,4 +1,5 @@
 import random as rand
+import itertools
 from abc import ABC, abstractmethod, staticmethod
 
 #COLORS = {1:"Purple", 2:"Brown", 3:"Red", 4:"Yellow", 5:"Green", 6:"Blue"}
@@ -46,6 +47,8 @@ class Board():
         # index_of_fakes["{num}{color}"] all places on board as (player,stone) tuples with a fake - Card(num,color,True)
         self.cards_on_board = [False for _ in range(NUM_OF_NUMS*NUM_OF_COLORS)] 
         # cards_on_board[k*NUM_OF_COLORS + j] == True if and only if card with color j+1 and num k+1 is on the board
+        self.fake_generator = [[Board.__best_cards_with_zero_placed_gen() for _ in range(NUM_OF_STONES)] for x in range(2)]
+        # next(self.fake_generator[player][stone]) = next best fakes you can add to location [player][stone]
     
     @staticmethod
     def convert_to_cards_on_board_index(num: int, color: int):
@@ -63,49 +66,42 @@ class Board():
             if type(self.cards[player][stone][i]) == Card and self.cards[player][stone][i].fake == True: 
                 self.cards[player][stone] = self.cards[player][stone][:i] # real cards are always at the beginning
                 return
+            
+    def __best_card_with_two_placed_gen(self,cards: list(tuple(int, int))):
+        num1, color1, num2, color2 = cards[0], cards[1]
+        diff = abs(num1-num2)
+        run = diff > 0 and diff <= 2
 
-    def __best_card_with_two_placed(self, player: int, stone: int):
-        """logic for best card available when two are already in place
-            returns tuple(num: int, color: int) of a possible fake to add"""
-        diff = abs(self.cards[player][stone][0].num - self.cards[player][stone][1].num)
-        color = False
+        if diff == 0:
+            # try three of a kind
+            possible_colors = filter(lambda x: x != color1 and x != color2,
+                                      (c for c in range(1, NUM_OF_COLORS + 1)))
+            for col in possible_colors:
+                yield [(num1, col)]
 
-        if self.cards[player][stone][0].color == self.cards[player][stone][1].color:
-            # color or color run is possible
-            color = True
-
-        if diff == 0: 
-            # then only three of a kind is possible
-            index = Board.convert_to_cards_on_board_index(self.cards[player][stone][0].num,1)
-            for j in range(index, index+NUM_OF_COLORS):
-                if self.cards_on_board[j] == False:
-                    return (self.cards[player][stone][0].num, j+1)
-        elif diff <= 2:
-            # a run may be possible
-            possible_nums = list(filter(
-                lambda x: abs(self.cards[player][stone][0].num - x) <= 2 and abs(self.cards[player][stone][1].num - x) <= 2,
-                (n for n in range(1, NUM_OF_NUMS + 1))))
-            possible_nums.sort(reverse=True) # try best option of run first
-            if color:
-                # a color run may be possible
-                for n in possible_nums:
-                    if self.cards_on_board[Board.convert_to_cards_on_board_index(n, self.cards[player][stone][0].color)] == False:
-                        return (n, self.cards[player][stone][0].color)
-                # else a color might be possible 
-                for k in range(1, NUM_OF_NUMS + 1):
-                    if self.cards_on_board[Board.convert_to_cards_on_board_index(k,self.cards[player][stone][0].color)] == False:
-                        return (k, self.cards[player][stone][0].color)
-            else:
-                # only a run might be available
-                for n in possible_nums:
-                    index = Board.convert_to_cards_on_board_index(n,1)
-                    for j in range(NUM_OF_COLORS):
-                        if self.cards_on_board[index+j] == False:
-                            return (n,j+1)
-        # only sum is available
-        for i in range(len(self.cards_on_board)-1, -1, -1):
-            if self.cards_on_board[i] == False:
-                return ((i // NUM_OF_COLORS)+1, (i % NUM_OF_COLORS)+1)
+        else:
+            if color1 == color2: # is color\color run possible?
+                # try color run
+                if run:
+                    possible_runs = filter(lambda x: abs(num1-x) <= 2 and abs(num2-x) <= 2 and x > 0 and x <= NUM_OF_NUMS,
+                                            set([num1+1, num1-1, num2+1, num2-1].sorted(reverse=True)))
+                    for possible_run in possible_runs:
+                        yield [(possible_run, color1)]
+                # then try color 
+                for num in filter(lambda x: x != num2 and x != num1, range(NUM_OF_NUMS,0,-1)):
+                    yield [(num, color1)]
+            # then run
+            if run:
+                possible_runs = filter(lambda x: abs(num1-x) <= 2 and abs(num2-x) <= 2 and x > 0 and x <= NUM_OF_NUMS,
+                                        set([num1+1, num1-1, num2+1, num2-1].sorted(reverse=True)))
+                for possible_run in possible_runs:
+                    for col in range(NUM_OF_COLORS, 0, -1):
+                        yield [(possible_run, col)]
+        # finally sum
+        for n in range(NUM_OF_NUMS, 0, -1):
+            for c in range(NUM_OF_COLORS, 0, -1):
+                yield [(n,c)]
+        return
 
     def __best_cards_with_one_placed_gen(self, num: int, color: int):
         """logic for best card available when one is already in place
@@ -115,7 +111,10 @@ class Board():
         for possible_run in POSSIBLE_RUNS:
             yield [(possible_run[0], color),(possible_run[1], color)]
         # try three of a kind 
-
+        filter_color = lambda x: x != color
+        for c in filter(filter_color, range(2, NUM_OF_COLORS + 1)):
+            for cc in filter(filter_color, range(1,c)):
+                yield [(num, c), (num, cc)]
         # generate best color combinations by order
         j,k = NUM_OF_NUMS,NUM_OF_NUMS - 1
         # MIGHT NOT WORK IF NUM_OF_NUMS IS EVEN 
@@ -167,6 +166,7 @@ class Board():
                 else:
                     j -= 1
                     k += 1
+        return
 
     @staticmethod
     def __color_combinations():
@@ -175,6 +175,48 @@ class Board():
             for j in range(2,i):
                 for k in range(1,j):
                     yield(i,j,k)
+
+    @staticmethod
+    def __compute_max_sum(n,r,up_limit):
+        q = n // r
+        p = n % r
+        return r*(q*up_limit - (q*(q-1))//2) + (p)*(up_limit-q)
+    
+    @staticmethod
+    def __compute_min_sum(n,r,down_limit):
+        q = n // r
+        p = n % r
+        return r*(down_limit*q+(q*(q-1))//2) + p*(down_limit+q)
+
+    @staticmethod
+    def __equal_sum_combinations(s: int,n: int,r: int,up_limit: int,low_limit: int):
+        """generates, without repetitions, all combinations of n integers,
+            all of which are between up_limit and low_limit (including), with at most r instances of the same number,
+            that add up to sum"""
+        if n == 0 or up_limit < low_limit:
+            yield []
+            return
+        if n == 1:
+            assert up_limit >= s and low_limit <= s
+            yield [s]
+            return
+        assert up_limit > 0 and low_limit > 0 and up_limit >= low_limit
+        min_min_int = max(s - Board.__compute_max_sum(n-1,r,up_limit),low_limit)
+        max_min_int = max(up_limit - (n//r) + 1, low_limit)
+        q = n // r
+        p = n % r
+        assert (p==1 or up_limit-q+1>=low_limit) and (p==0 or up_limit-q>=low_limit) # assert that there is enough numbers
+        for rep in range(1, min(r + 1,n + 1)):
+            for num in range(min_min_int, max_min_int+1):
+                if Board.__compute_min_sum(n-rep,r,num+1) + num*rep <= s\
+                and Board.__compute_max_sum(n-rep,r,up_limit) + num*rep >= s:
+                    for combination in Board.__equal_sum_combinations(s-(num*rep),n-rep,r,up_limit,num+1):
+                        yield [num for _ in range(rep)] + combination
+        return
+
+        
+        
+
 
     def __best_cards_with_zero_placed_gen(self):
         # try color run
@@ -187,7 +229,20 @@ class Board():
             for combination in Board.__color_combinations():
                 yield [(num, c) for c in combination]
         # generate best color combinations by order
-
+        for s in range(3*NUM_OF_NUMS-3, 5, -1):  # assumes num_of_nums >= 3
+            for nums in Board.__equal_sum_combinations(s,3,1,NUM_OF_NUMS,1):
+                for c in range(1, NUM_OF_COLORS + 1):
+                    yield [(num,c) for num in nums]
+        # best runs
+        for possible_run in POSSIBLE_RUNS:
+            for colors in itertools.permutations(range(1,NUM_OF_COLORS+1), 3):
+                yield [(run[0],run[1]) for run in zip(possible_run,colors)]
+        # best sums
+        for s in range(3*NUM_OF_NUMS-1, 3, -1):  # assumes num_of_nums >= 3
+            for nums in Board.__equal_sum_combinations(s,3,2,NUM_OF_NUMS,1):
+                for colors in itertools.permutations(range(1,NUM_OF_COLORS+1), 3):
+                    yield [(comb[0],comb[1]) for comb in zip(nums,colors)]
+        return
         
 
     def get_best_hand(self, player: int, stone: int):
@@ -204,12 +259,11 @@ class Board():
                     self.cards_on_board[Board.convert_to_cards_on_board_index(options[1][0], options[1][1])] == False:
                     return options
         if cards_in_place == 0:
-            return
-
-
-                
-
-                
+            for options in self.__best_cards_with_zero_placed_gen():
+                if self.cards_on_board[Board.convert_to_cards_on_board_index(options[0][0], options[0][1])] == False and\
+                    self.cards_on_board[Board.convert_to_cards_on_board_index(options[1][0], options[1][1])] == False and\
+                        self.cards_on_board[Board.convert_to_cards_on_board_index(options[2][0], options[2][1])] == False:
+                        return options                
 
     def update_fakes(self, card: Card):
         """deletes all fake occurrences of card
